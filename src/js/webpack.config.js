c8693fa016328121f69713b65ce6a4a36f1fe17b
Kamil Matejuk <kamilmatejuk@gmail.com>
2019-12-09 17:29:35 +0100

    Configured first version of webpack

--- src/js/webpack.config.js
+++ src/js/webpack.config.js
@@ -1,19 +1,24 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // https://github.com/babel/babel-loader            <-  kompresja js
 // https://github.com/jantimon/html-webpack-plugin  <-  tworzenie html z danym js
+// https://github.com/webpack-contrib/mini-css-extract-plugin   <-  tworzenie html z danym css
 // https://github.com/webpack/webpack-dev-server    <-  praca na żywo <- uruchamiane za pomocą 'npm run dev',
 //                                                      potem trzeba przejść pod odpowiedni adres jaki sie wyświetli 
 //                                                      (prawdopodobnie 'http://localhost:8080/')
 
 module.exports = {
     entry: {
+        //  W TYM MIEJSCU DODAJEMY ZALEŻNE JS I CSS, A NIE W HTML
         loginPage: [
-            './start.js'
+            './start.js',
+            '../css/index.css'
         ],
         mainPage: [
-            './to-do-list-main.js'
+            './to-do-list-main.js',
+            '../css/to-do-list.css'
         ]
     },
     output: {
@@ -22,17 +27,54 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: "file-loader",
+                    options: {}
+                }]
+            },
+            {
+                test: /\.svg$/,
+                loader: 'url-loader?limit=65000&mimetype=image/svg+xml&name=font/[name].[ext]'
+            },
+            {
+                test: /\.woff$/,
+                loader: 'url-loader?limit=65000&mimetype=application/font-woff&name=font/[name].[ext]'
+            },
+            {
+                test: /\.woff2$/,
+                loader: 'url-loader?limit=65000&mimetype=application/font-woff2&name=font/[name].[ext]'
+            },
+            {
+                test: /\.[ot]tf$/,
+                loader: 'url-loader?limit=65000&mimetype=application/octet-stream&name=font/[name].[ext]'
+            },
+            {
+                test: /\.eot$/,
+                loader: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=font/[name].[ext]'
             }
-        }]
+        ]
     },
     plugins: [
+        new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: '../html/index.html',
