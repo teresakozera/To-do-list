f4c73503d5a8c84ef371a095f7fa7694d611b0fa
Kamil Matejuk <kamilmatejuk@gmail.com>
2019-12-09 16:44:52 +0100

    Added babel

--- src/js/webpack.config.js
+++ src/js/webpack.config.js
@@ -1,12 +1,41 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        loginPage:  './start.js',
-        listPage:   './to-do-list-main.js'
+        loginPage: [
+            './start.js'
+        ],
+        mainPage: [
+            './to-do-list-main.js'
+        ]
     },
     output: {
         path: path.resolve(__dirname, '../../dist'),
         filename: '[name].js'
-    }
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: '../html/index.html',
+            chunks: ['loginPage']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'main.html',
+            template: '../html/to-do-list-main.html',
+            chunks: ['mainPage']
+        })
+    ]
 };
