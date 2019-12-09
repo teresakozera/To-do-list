a0d18624407a3f934b8e84224c9bd004d3f7005f
Kamil Matejuk <kamilmatejuk@gmail.com>
2019-12-09 16:57:42 +0100

    Added life server

--- src/js/webpack.config.js
+++ src/js/webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// https://github.com/babel/babel-loader            <-  kompresja js
+// https://github.com/jantimon/html-webpack-plugin  <-  tworzenie html z danym js
+// https://github.com/webpack/webpack-dev-server    <-  praca na żywo <- uruchamiane za pomocą 'npm run dev',
+//                                                      potem trzeba przejść pod odpowiedni adres jaki sie wyświetli 
+//                                                      (prawdopodobnie 'http://localhost:8080/')
+
 module.exports = {
     entry: {
         loginPage: [
