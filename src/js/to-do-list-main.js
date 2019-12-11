ecba7eeb5296ac450f570aae4233f11df1cb0207
Teresa Kozera <tekozera@gmail.com>
2019-12-12 00:28:31 +0100

    Completed UI (HTML and CSS). Mobile version added. Changes in the logic in js.

--- src/js/to-do-list-main.js
+++ src/js/to-do-list-main.js
@@ -3,27 +3,35 @@ const LISTS_LIMIT = 7;
 
 let listIndex = 0;
 const plusListBtn = document.getElementById('plus-list').addEventListener('click', (e) => {
-    if(event.target.parentNode.parentNode.children.length < LISTS_LIMIT) {
-        let divList = document.createElement('div');
+    if(event.currentTarget.parentNode.parentNode.children.length < LISTS_LIMIT) {
+        const divList = document.createElement('div');
+        $(divList).addClass('list');
+        // add divList#number- depending on how many iteams are aldready on the page
+        $(divList).addClass(`list${event.currentTarget.parentNode.parentNode.children.length}`);
 
-        let listTitle = document.createElement('p');
-        let plusBtn = document.createElement('BUTTON');
+        const listTitle = document.createElement('p');
+        listTitle.innerHTML = `List${event.currentTarget.parentNode.parentNode.children.length}`;
+    
+        const listHeader = document.createElement('div');
+        $(listHeader).addClass('list-header');
+        listHeader.appendChild(listTitle);
 
-        listTitle.innerHTML = `List${event.target.parentNode.parentNode.children.length}`;
+        divList.appendChild(listHeader);
+
+        const listItems = document.createElement('div');
+        $(listItems).addClass('list-items');
+
+        const plusBtn = document.createElement('BUTTON');
         $(plusBtn).addClass("icon-plus-outline");
         plusBtn.innerHTML = "Add another item";        
         $(plusBtn).addClass("plus-task");
 
-        divList.appendChild(listTitle);
+        divList.appendChild(listItems);
         divList.appendChild(plusBtn);
-
-        $(divList).addClass('list');
-        // add divList#number- depending on how many iteams are aldready on the page
-        $(divList).addClass(`list${event.target.parentNode.parentNode.children.length}`);
         
         // add a div to the DOM
         // move the '+Add a list' button to the right of newly inserted list
-        event.target.parentNode.parentNode.insertBefore(divList, event.target.parentNode);
+        event.currentTarget.parentNode.parentNode.insertBefore(divList, event.currentTarget.parentNode);
         listIndex++;
     } else {
         alert('Too many lists created. You can have up to six lists active!');
@@ -39,10 +47,10 @@ $(document).on('click','.plus-task', e => {
     const index = event.target.parentNode.children.length-1;
 
     const divItem = document.createElement('div');
+    console.log(`list${listIndex}todo-item${index}`);
     // every div has a unique id- the information needed for the CRUD operations
     divItem.id=`list${listIndex}todo-item${index}`;
     $(divItem).addClass('item');
-    // $(divItem).addClass(`item${index}`);
 
     // create an item for the to-do list!
     const listItem = document.createElement('INPUT');
@@ -64,13 +72,17 @@ $(document).on('click','.plus-task', e => {
     divItem.appendChild(labelItem);
     divItem.appendChild(deleteButton);
 
-    e.target.parentNode.insertBefore(divItem, e.target);
+    $(event.target).parents(`.list`).first().children('.list-items').first().append(divItem);
 });
 
 $(document).on('click','.minus-task', e => {
     // needed to retrieve the information for the db
-    const deletedDivId =e.target.parentNode.id;
+    const deletedDivId =e.currentTarget.parentNode.id;
 
-    let divToDelete = e.target.parentNode;
+    let divToDelete = e.currentTarget.parentNode;
     divToDelete.remove();
+});
+
+const logoutBtn = document.getElementById('logout').addEventListener('click', (e) => {
+    alert('Should log out!');
 });
