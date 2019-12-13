14eddebb372585bb3848fc76a58a86c14de8cbcb
Kamil Matejuk <kamilmatejuk@gmail.com>
2019-12-13 20:04:16 +0100

    Changed ports

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,13 @@
 const config = require('config');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const users = require('./routes/users.js');
 const auth = require('./routes/auth.js');
 const express = require('express');
 const app = express();
 
 // najpierw trzeba ustawic zmienna globalna
-//set todolist_jwtPrivateKey=mySecureKey
+// set todolist_jwtPrivateKey=mySecureKey
 if(!config.get('jwtPrivateKey')){
     console.log('FATAL ERROR: jwtPrivateKey is not defined');
     process.exit(1);
@@ -21,6 +22,7 @@ mongoose.connect('mongodb://localhost/to_do_list', {
     .catch(err => console.error('could not connect', err));
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 
--- src/js/register.js
+++ src/js/register.js
@@ -5,30 +5,46 @@ document.getElementById('registerForm').addEventListener('submit', (e) => {
     const pass = document.getElementById('reg_psw').value;
     const pass_rep = document.getElementById('reg_psw_repeat').value;
 
-    if(pass != pass_rep) {
+    if (pass != pass_rep) {
         return alert('Oba hasła muszą być takie same');
     }
 
-    const usr = {
+    const usr = JSON.stringify({
         email: mail,
         password: pass
-    };
-    
+    });
+
     // TODO nauczyć się wysyłać zapytania
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://localhost:3000/api/users/", true);
-    xhr.send(JSON.stringify(usr));
-    console.log(xhr.response);
+    xhr.onloadend = function () {
+        alert(this.status);
+    };
+    xhr.open("POST", "https://localhost:3000/api/users/");
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.send(usr);
+    
 });
 
 // login clicked
 document.getElementById('loginForm').addEventListener('submit', (e) => {
     e.preventDefault();
-    const email = document.getElementById('log_email').value;
+    const mail = document.getElementById('log_email').value;
     const pass = document.getElementById('log_psw').value;
 
+    const usr = JSON.stringify({
+        email: mail,
+        password: pass
+    });
+
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://localhost:3000/api/users/me", true);
-    xhr.send(null);
-    
+    xhr.withCredentials = true;
+    xhr.open("POST", "https://localhost:3000/api/auth");
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            alert(xhr.status);
+        }
+    };
+    xhr.send(usr);
+
 });
